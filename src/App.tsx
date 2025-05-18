e6af7ab3ef9407af3da0abcc5f54f41f60fe10c2
Richard Jimenez <rjimenez.gambarte@gmail.com>
2025-05-18 18:32:39 -0400

    Version 1.0

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+
+  // jsx traduce el html a js
+  // tsx es un tipo de archivo que permite escribir html dentro de js
+  return (
+    <p>Hola Mundo</p>
+  );
+}
+export default App;
