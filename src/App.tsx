e29b2a5efe1c1ceb429b820b357ad1f604fe2fd7
Richard Jimenez <rjimenez.gambarte@gmail.com>
2025-05-18 22:04:16 -0400

    Version 1.005 Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import Card from "./components/Card";
+import Card, { CardBody } from "./components/Card";
 
 function App() {
-  return <Card />;
+  // return <Card  body={"Hola Mundo"} />;
+  return <CardBody title={"Hola Mundo"} text={"Texto del componente"} />;
 }
 
 export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,11 @@
 import { Fragment } from "react/jsx-runtime";
 
-function Card() {
+interface CardProps {
+  body: string;
+}
+
+function Card(props: CardProps) {
+  const { body } = props;
   return (
     <div
       className="card"
@@ -9,22 +14,24 @@ function Card() {
       }}
     >
       <h5 className="card-header">Featured</h5>
-      <div className="card-body">
-        <CardBody />
-      </div>
+      <div className="card-body">{body}</div>
     </div>
   );
 }
 export default Card;
 
-export function CardBody() {
+interface CardBodyProps {
+  title: string;
+  text?: string;
+}
+
+export function CardBody(props: CardBodyProps) {
+  const { title, text } = props;
   return (
     <Fragment>
       <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
         <a href="#" className="btn btn-primary">
           Go somewhere
         </a>
